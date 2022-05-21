afe0f2fe53471db5c3a03c451d074de3ea7f93da
usefulUS3RL3SS <78636744+usefulUS3RL3SS@users.noreply.github.com>
2022-05-21 14:33:30 +0700

    Added Lab0

---
+++ Lab0/lab0/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab0.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
