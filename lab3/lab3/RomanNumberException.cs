61d601c7d1698698c9d99fb19281302e9376a755
usefulUS3RL3SS <78636744+usefulUS3RL3SS@users.noreply.github.com>
2022-05-21 15:44:50 +0700

    Added Lab3

---
+++ lab3/lab3/RomanNumberException.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+public class RomanNumberException : Exception
+{
+    public RomanNumberException() : base() { }
+    public RomanNumberException(string message) : base(message) { }
+}
